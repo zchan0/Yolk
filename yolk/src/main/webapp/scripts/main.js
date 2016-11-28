49510af6c3e843ea75bba0e4afc06945a63b9482
zchan0 <zchan0@outlook.com>
2016-11-28 02:36:31 -0500

    Use ajax

--- yolk/src/main/webapp/scripts/main.js
+++ yolk/src/main/webapp/scripts/main.js
@@ -1,1 +1,1 @@
-"use strict";
+"use strict";$("#signupBtn").click(function(){var n=$("form").serialize(),e=$.ajax({type:"POST",data:n,url:"/user/signup.json",success:function(n){console.log(n),alert("Signup Succeed!")}});e.error(function(){alert("Something went wrong")})}),$("#loginBtn").click(function(){var n=$("form").serialize(),e=$.ajax({type:"POST",data:n,url:"/user/login.json",success:function(n){console.log(n),alert("login Succeed")}});e.error(function(){alert("Something went wrong")})});
