a8a6530437b8b6852fd38b6c4aedec12939ac390
zchan0 <zchan0@outlook.com>
2016-11-30 13:11:10 -0500

    Fix typo & Resolve conflicts

--- yolk/src/main/webapp/scripts/main.js
+++ yolk/src/main/webapp/scripts/main.js
@@ -1,25 +1,3 @@
-"use strict";
-$("#signupBtn").click(function () {
-    var n = $("form").serialize(), e = $.ajax({
-        type: "POST", data: n, url: "/user/signup.json", success: function (n) {
-            console.log(n), alert("Signup Succeed!")
-        }
-    });
-    e.error(function () {
-        alert("Something went wrong")
-    })
-}), $("#loginBtn").click(function () {
-    var n = $("form").serialize(), e = $.ajax({
-        type: "POST", data: n, url: "/user/login.json", success: function (n) {
-            console.log(n), alert("login Succeed")
-        }
-    });
-    e.error(function () {
-        alert("Something went wrong")
-    })
-});
-'use strict';
-
 // login & signup
 
 $('#signupBtn').click(function () {
@@ -61,7 +39,6 @@ $('#loginBtn').click(function () {
 });
 
 (function ($) {
-
 	var $container = $('.masonry-container');
 	$container.imagesLoaded(function () {
 		$container.masonry({
