e30b0e671fc8e679632d78f35d16893564d9c47d
sqct2008 <sqct2008@gmail.com>
2016-12-05 00:57:53 -0500

    update

--- yolk/src/main/webapp/scripts/vendor.js
+++ yolk/src/main/webapp/scripts/vendor.js
@@ -11627,7 +11627,7 @@ window.Modernizr = (function( window, document, undefined ) {
 })(this, this.document);
 
 /*!
- * jQuery Validation Plugin v1.15.1
+ * jQuery Validation Plugin v1.16.0
  *
  * http://jqueryvalidation.org/
  *
@@ -11832,7 +11832,7 @@ $.extend( $.fn, {
 } );
 
 // Custom selectors
-$.extend( $.expr[ ":" ], {
+$.extend( $.expr.pseudos || $.expr[ ":" ], {		// '|| $.expr[ ":" ]' here enables backwards compatibility to jQuery 1.7. Can be removed when dropping jQ 1.7.x support
 
 	// http://jqueryvalidation.org/blank-selector/
 	blank: function( a ) {
@@ -12045,7 +12045,7 @@ $.extend( $.validator, {
 					":text, [type='password'], [type='file'], select, textarea, [type='number'], [type='search'], " +
 					"[type='tel'], [type='url'], [type='email'], [type='datetime'], [type='date'], [type='month'], " +
 					"[type='week'], [type='time'], [type='datetime-local'], [type='range'], [type='color'], " +
-					"[type='radio'], [type='checkbox'], [contenteditable]", delegate )
+					"[type='radio'], [type='checkbox'], [contenteditable], [type='button']", delegate )
 
 				// Support: Chrome, oldIE
 				// "select" is provided as event.target when clicking a option
@@ -13198,7 +13198,7 @@ if ( $.ajaxPrefilter ) {
 		return ajax.apply( this, arguments );
 	};
 }
-
+return $;
 }));
 /*!
  * Masonry PACKAGED v4.1.1
@@ -16301,3 +16301,120 @@ return ImagesLoaded;
 
 	return init(function () {});
 }));
+
+/**
+ * AJAX File Upload
+ * http://github.com/davgothic/AjaxFileUpload
+ * 
+ * Copyright (c) 2010-2013 David Hancock (http://davidhancock.co)
+ *
+ * Thanks to Steven Barnett for his generous contributions
+ *
+ * Licensed under the MIT license ( http://www.opensource.org/licenses/MIT )
+ */
+
+;(function($) {
+	$.fn.AjaxFileUpload = function(options) {
+		
+		var defaults = {
+			action:     "upload.php",
+			onChange:   function(filename) {},
+			onSubmit:   function(filename) {},
+			onComplete: function(filename, response) {}
+		},
+		settings = $.extend({}, defaults, options),
+		randomId = (function() {
+			var id = 0;
+			return function () {
+				return "_AjaxFileUpload" + id++;
+			};
+		})();
+		
+		return this.each(function() {
+			var $this = $(this);
+			if ($this.is("input") && $this.attr("type") === "file") {
+				$this.bind("change", onChange);
+			}
+		});
+		
+		function onChange(e) {
+			var $element = $(e.target),
+				id       = $element.attr('id'),
+				$clone   = $element.removeAttr('id').clone().attr('id', id).AjaxFileUpload(options),
+				filename = $element.val().replace(/.*(\/|\\)/, ""),
+				iframe   = createIframe(),
+				form     = createForm(iframe);
+
+			// We append a clone since the original input will be destroyed
+			$clone.insertBefore($element);
+
+			settings.onChange.call($clone[0], filename);
+
+			iframe.bind("load", {element: $clone, form: form, filename: filename}, onComplete);
+			
+			form.append($element).bind("submit", {element: $clone, iframe: iframe, filename: filename}, onSubmit).submit();
+		}
+		
+		function onSubmit(e) {
+			var data = settings.onSubmit.call(e.data.element, e.data.filename);
+
+			// If false cancel the submission
+			if (data === false) {
+				// Remove the temporary form and iframe
+				$(e.target).remove();
+				e.data.iframe.remove();
+				return false;
+			} else {
+				// Else, append additional inputs
+				for (var variable in data) {
+					$("<input />")
+						.attr("type", "hidden")
+						.attr("name", variable)
+						.val(data[variable])
+						.appendTo(e.target);
+				}
+			}
+		}
+		
+		function onComplete (e) {
+			var $iframe  = $(e.target),
+				doc      = ($iframe[0].contentWindow || $iframe[0].contentDocument).document,
+				response = doc.body.innerHTML;
+
+			if (response) {
+				response = $.parseJSON(response);
+			} else {
+				response = {};
+			}
+
+			settings.onComplete.call(e.data.element, e.data.filename, response);
+			
+			// Remove the temporary form and iframe
+			e.data.form.remove();
+			$iframe.remove();
+		}
+
+		function createIframe() {
+			var id = randomId();
+
+			// The iframe must be appended as a string otherwise IE7 will pop up the response in a new window
+			// http://stackoverflow.com/a/6222471/268669
+			$("body")
+				.append('<iframe src="javascript:false;" name="' + id + '" id="' + id + '" style="display: none;"></iframe>');
+
+			return $('#' + id);
+		}
+		
+		function createForm(iframe) {
+			return $("<form />")
+				.attr({
+					method: "post",
+					action: settings.action,
+					enctype: "multipart/form-data",
+					target: iframe[0].name
+				})
+				.hide()
+				.appendTo("body");
+		}
+	};
+})(jQuery);
